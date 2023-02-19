013e2635564395c1dd346a0b576e86810ae42fb3
whitebm2024 <77416489+whitebm2024@users.noreply.github.com>
2023-02-19 07:26:10 -0500

    Formatting

--- src/main/java/dfa05/DFA.java
+++ src/main/java/dfa05/DFA.java
@@ -1,4 +1,3 @@
-
 package dfa05;
 
 
@@ -14,22 +13,21 @@ Brandon White, spring 2023
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-        
-        
+
 import javax.swing.JTextArea;
 
 /*need a public void run() method to implement runnable*/
-public class DFA implements Runnable  {
-    
+public class DFA implements Runnable {
+
     private int count;
     private int ms_to_sleep;
     private boolean pause;
     private Thread thread;
     private JTextArea textArea;
-    
+
     //pass a JTextArea in so that run() can manipulate the textArea
     DFA(JTextArea textArea) {
-        
+
         count = 0;
         ms_to_sleep = 500;
         pause = true;
@@ -37,7 +35,7 @@ public class DFA implements Runnable  {
         thread.start();
         this.textArea = textArea;
     }
-    
+
     public void reset() {
         pause = true;
         count = 0;
@@ -46,147 +44,144 @@ public class DFA implements Runnable  {
     @Override
     /*need a public void run() method to implement runnable*/
     public void run() {
-        
-         while (true)
-        {
-            if ( ! pause )
-            {
+
+        while (true) {
+            if (!pause) {
                 singleStep();
                 textArea.append("Count: " + this.count + '\n');
             }
-            
-            try
-            {
+
+            try {
                 thread.sleep(ms_to_sleep);
-            }catch (Exception ex) {}
+            } catch (Exception ex) {
+            }
         }
     }
-    
+
     public void singleStep() {
         count += 1;
-        System.out.println("count:  "+count);
+        System.out.println("count:  " + count);
     }
-    
+
     public void setPause(boolean pause) {
         this.pause = pause;
     }
-    
-    public int getCount(){
+
+    public int getCount() {
         return count;
     }
-    public void setCount(int i){
+
+    public void setCount(int i) {
         count = i;
     }
-    public boolean getPause(){
+
+    public boolean getPause() {
         return pause;
     }
-    public void setSleep(int i){
+
+    public void setSleep(int i) {
         ms_to_sleep = i;
     }
-    public int getSleep(){
+
+    public int getSleep() {
         return ms_to_sleep;
     }
-    
-public boolean validateDfa(String input) {
-    String[] lines = input.split("\\r?\\n");
-    List<String> filteredLines = new ArrayList<>();
 
-    // Filter out comment lines and remove superfluous whitespace
-    for (String line : lines) {
-        String trimmedLine = line.trim();
-        if (!trimmedLine.startsWith("//") && !trimmedLine.isEmpty()) {
-            filteredLines.add(trimmedLine.replaceAll("\\s+", " "));
+    public boolean validateDfa(String input) {
+        String[] lines = input.split("\\r?\\n");
+        List<String> filteredLines = new ArrayList<>();
+
+        // Filter out comment lines and remove superfluous whitespace
+        for (String line : lines) {
+            String trimmedLine = line.trim();
+            if (!trimmedLine.startsWith("//") && !trimmedLine.isEmpty()) {
+                filteredLines.add(trimmedLine.replaceAll("\\s+", " "));
+            }
         }
-    }
 
-    // Check the number of lines
-    if (filteredLines.size() != 5) {
-        textArea.append("//invalid number of non comment lines (must be 5)\n");
-        return false;
-    }
+        // Check the number of lines
+        if (filteredLines.size() != 5) {
+            textArea.append("//invalid number of non comment lines (must be 5)\n");
+            return false;
+        }
 
         // Check the format of each line
-    // Line 1: set of states
-    String[] states = filteredLines.get(0).split(" ");
-    if (states.length < 1) {
-        textArea.append("//invalid number of states\n");
-        return false;
-    }
-    for (String state : states) {
-        if (!state.matches("^[a-zA-Z]\\w{0,9}$")) {
-            textArea.append("//invalid state name '" + state + "'\n");
+        // Line 1: set of states
+        String[] states = filteredLines.get(0).split(" ");
+        if (states.length < 1) {
+            textArea.append("//invalid number of states\n");
             return false;
         }
-    }
+        for (String state : states) {
+            if (!state.matches("^[a-zA-Z]\\w{0,9}$")) {
+                textArea.append("//invalid state name '" + state + "'\n");
+                return false;
+            }
+        }
 
-    // Line 2: input alphabet
-    String[] inputAlphabet = filteredLines.get(1).split(" ");
-    if (inputAlphabet.length < 1) {
-        textArea.append("//invalid number of input symbols\n");
-        return false;
-    }
-    for (String symbol : inputAlphabet) {
-        if (!symbol.matches("^\\w$")) {
-            textArea.append("//invalid input symbol '" + symbol + "'\n");
+        // Line 2: input alphabet
+        String[] inputAlphabet = filteredLines.get(1).split(" ");
+        if (inputAlphabet.length < 1) {
+            textArea.append("//invalid number of input symbols\n");
             return false;
         }
-    }
+        for (String symbol : inputAlphabet) {
+            if (!symbol.matches("^\\w$")) {
+                textArea.append("//invalid input symbol '" + symbol + "'\n");
+                return false;
+            }
+        }
 
-    // Line 3: initial state
-    String initialState = filteredLines.get(2);
-    if (!initialState.matches("^[a-zA-Z]\\w{0,9}$")) {
-        textArea.append("//invalid initial state name '" + initialState + "'\n");
-        return false;
-    }
-    if (!Arrays.asList(states).contains(initialState)) {
-        textArea.append("//start state not in state list -- '" + initialState + "'\n");
-        return false;
-    }
+        // Line 3: initial state
+        String initialState = filteredLines.get(2);
+        if (!initialState.matches("^[a-zA-Z]\\w{0,9}$")) {
+            textArea.append("//invalid initial state name '" + initialState + "'\n");
+            return false;
+        }
+        if (!Arrays.asList(states).contains(initialState)) {
+            textArea.append("//start state not in state list -- '" + initialState + "'\n");
+            return false;
+        }
 
-    // Line 4: set of favorable states
-    String[] favorableStates = filteredLines.get(3).split(" ");
-    if (favorableStates.length < 1) {
-        textArea.append("//invalid number of favorable states\n");
-        return false;
-    }
-    for (String state : favorableStates) {
-        if (!state.matches("^[a-zA-Z]\\w{0,9}$")) {
-            textArea.append("//invalid favorable state name '" + state + "'\n");
+        // Line 4: set of favorable states
+        String[] favorableStates = filteredLines.get(3).split(" ");
+        if (favorableStates.length < 1) {
+            textArea.append("//invalid number of favorable states\n");
             return false;
         }
-        if (!Arrays.asList(states).contains(state)) {
-            textArea.append("//favorable state not in state list -- '" + state + "'\n");
-            return false;
+        for (String state : favorableStates) {
+            if (!state.matches("^[a-zA-Z]\\w{0,9}$")) {
+                textArea.append("//invalid favorable state name '" + state + "'\n");
+                return false;
+            }
+            if (!Arrays.asList(states).contains(state)) {
+                textArea.append("//favorable state not in state list -- '" + state + "'\n");
+                return false;
+            }
         }
-    }
 
-    // Line 5: transition table
-    String[] table = filteredLines.get(4).split(" ");
-    if (table.length != states.length * inputAlphabet.length) {
-        textArea.append("//invalid number of transitions\n");
-        return false;
-    }
-    for (int i = 0; i < table.length; i++) {
-        String state = states[i / inputAlphabet.length];
-        String symbol = inputAlphabet[i % inputAlphabet.length];
-        String nextState = table[i];
-        if (!nextState.matches("^[a-zA-Z]\\w{0,9}$")) {
-            textArea.append("//invalid next state name '" + nextState + "'\n");
+        // Line 5: transition table
+        String[] table = filteredLines.get(4).split(" ");
+        if (table.length != states.length * inputAlphabet.length) {
+            textArea.append("//invalid number of transitions\n");
             return false;
         }
-        if (!Arrays.asList(states).contains(nextState)) {
-            textArea.append("//next state not in state list -- '" + nextState + "'\n");
-            return false;
+        for (int i = 0; i < table.length; i++) {
+            String state = states[i / inputAlphabet.length];
+            String symbol = inputAlphabet[i % inputAlphabet.length];
+            String nextState = table[i];
+            if (!nextState.matches("^[a-zA-Z]\\w{0,9}$")) {
+                textArea.append("//invalid next state name '" + nextState + "'\n");
+                return false;
+            }
+            if (!Arrays.asList(states).contains(nextState)) {
+                textArea.append("//next state not in state list -- '" + nextState + "'\n");
+                return false;
+            }
         }
-    }
 
-    // All checks passed
-    textArea.append("//valid dfa\n");
-    return true;
+        // All checks passed
+        textArea.append("//valid dfa\n");
+        return true;
+    }
 }
-}
-
-
-
-    
-
